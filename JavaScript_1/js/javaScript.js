212489925ac1626fc506c5aa9583b360b101487b
git-sandoval98 <sergioa.munoz@udla.edu.co>
2025-09-15 10:47:48 -0500

    carpeta react_v1

--- JavaScript_1/js/javaScript.js
+++ JavaScript_1/js/javaScript.js
@@ -56,3 +56,100 @@ if (a > b) {
 } else {
     console.log("a es igual que b");
 }
+
+//============================ FUNCIONES ============================
+
+function saludar(nombre, apellido) {
+    return "Hola " + nombre + " " + apellido;
+}
+
+console.log(saludar("Wilmer", "Patiño"));
+
+//============================ FUNCIONES ANONIMAS ============================
+
+const varA= function (x, y) {
+return x + y;
+}
+console.log( varA(3, 5));
+
+//============================ FUNCIONES FLECHA (Arrow Function) ============================
+
+const varB = () => {};
+const varC = (x) => {return x++;};
+const varD = x => {return x++;};
+const varE = (x, y) => {return x++ + y++;};
+const varF = x =>  x++ ;
+
+//============================ FUNCIONES DE ORDEN SUPERIOR ============================
+
+const arrayNum = [1,2,3,4,5,6,7,8,9,10];
+const arrayObj = [
+    {id:1, name:"Wilmer"},
+    {id:2, name:"Juan"},
+    {id:3, name:"Pedro"},
+    {id:4, name:"Maria"},
+    {id:5, name:"Ana"},
+];
+
+// es aquella que retorna o recibe como parametro otra función
+//map
+const arrayNum2 = arrayNum.map(  (elemento) =>{
+        return elemento * 2;
+    }   
+); 
+console.log(arrayNum2);
+
+//forEach
+arrayNum.forEach(  (elemento)=> {
+        console.log(elemento-1);
+    }   
+);
+
+//filter
+const arrayNum3 = arrayObj.filter(  (elemento) =>{
+        return elemento.name == "Pedro";
+    }   
+);
+console.log(arrayNum3);
+
+//find
+const arrayNum4 = arrayObj.find(  (elemento) =>{
+        return elemento.name == "Pedro";
+    }   
+);
+
+console.log(arrayNum4);
+
+//reduce
+const suma = arrayNum.reduce(  (acumulador, elemento) =>{
+        return acumulador + elemento;
+    }  , 10 
+);
+
+console.log(suma);
+
+//============================ OBJ - JSON ============================
+
+const persona = {
+    nombre: "Wilmer",
+    apellido: "Patiño", 
+    edad: 20,
+    direccion: {
+        calle: "Calle 123",
+        ciudad: "Bogotá",   
+        pais: "Colombia"
+    },
+    hobbies: ["Leer", "Programar", "Correr"],    
+};
+
+const persona2 = {
+    "nombre": "Ana",
+    "apellido": "Gomez",
+    "edad": 25,
+    "direccion": {
+        "calle": "Calle 456",
+        "ciudad": "Medellín",
+        "pais": "Colombia"
+    },
+    "hobbies": ["Bailar", "Cantar", "Viajar"],
+};
